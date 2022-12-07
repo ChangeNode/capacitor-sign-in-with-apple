01996411d6b7bb97ffbc765b94817a8be98142da
Will Iverson <wiverson@gmail.com>
2022-12-07 11:13:39 -0800

    Initial commit

---
+++ src/web.ts
@@ -0,0 +1,13 @@
+import { WebPlugin } from '@capacitor/core';
+
+import type { SignInWithApplePlugin } from './definitions';
+
+export class SignInWithAppleWeb
+  extends WebPlugin
+  implements SignInWithApplePlugin
+{
+  async echo(options: { value: string }): Promise<{ value: string }> {
+    console.log('ECHO', options);
+    return options;
+  }
+}
